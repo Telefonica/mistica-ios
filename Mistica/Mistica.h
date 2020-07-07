4bf12d9fb77108b60f963e6775de7777a396bfab
Pablo Bartolome <pbartolome@tuenti.com>
2020-07-07 10:43:22 +0200

    Initial commit, autogenerated project

---
+++ Mistica/Mistica.h
@@ -0,0 +1,19 @@
+//
+//  Mistica.h
+//  Mistica
+//
+//  Created by Pablo Bartolome on 07/07/2020.
+//  Copyright © 2020 Telefonica. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Mistica.
+FOUNDATION_EXPORT double MisticaVersionNumber;
+
+//! Project version string for Mistica.
+FOUNDATION_EXPORT const unsigned char MisticaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mistica/PublicHeader.h>
+
+

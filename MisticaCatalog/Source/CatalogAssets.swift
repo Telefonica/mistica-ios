fd3a7744ff70a4d9507c3207e2fb51cbc1e98a8f
Jose Miguel Brocal <jmbrocal@tuenti.com>
2020-08-10 17:29:34 +0200

    Set iPhoneOS deployment target to 11.0

--- MisticaCatalog/Source/CatalogAssets.swift
+++ MisticaCatalog/Source/CatalogAssets.swift
@@ -10,23 +10,23 @@ import Mistica
 import UIKit
 
 extension UIImage {
-    static let misticaLogo = UIImage(named: "mistica-logo", in: .misticaCatalog, with: nil)!
+    static let misticaLogo = UIImage(named: "mistica-logo", in: .misticaCatalog, compatibleWith: nil)!
 
-    static let buttonsIcon = UIImage(named: "Buttons", in: .misticaCatalog, with: nil)!
-    static let croutonIcon = UIImage(named: "Crouton", in: .misticaCatalog, with: nil)!
-    static let feedbacksIcon = UIImage(named: "Feedbacks", in: .misticaCatalog, with: nil)!
-    static let fontsIcon = UIImage(named: "Texts", in: .misticaCatalog, with: nil)!
-    static let headerIcon = UIImage(named: "Headers", in: .misticaCatalog, with: nil)!
-    static let inputFieldsIcon = UIImage(named: "InputFields", in: .misticaCatalog, with: nil)!
-    static let listIcon = UIImage(named: "Lists", in: .misticaCatalog, with: nil)!
-    static let popoverIcon = UIImage(named: "Popovers", in: .misticaCatalog, with: nil)!
-    static let scrollContentIndicatorIcon = UIImage(named: "Popovers", in: .misticaCatalog, with: nil)!
-    static let sectionTitleIcon = UIImage(named: "Lists", in: .misticaCatalog, with: nil)!
-    static let segmentSelectorIcon = UIImage(named: "Tabs", in: .misticaCatalog, with: nil)!
-    static let tagsIcon = UIImage(named: "Labels", in: .misticaCatalog, with: nil)!
-    static let viewStatesIcon = UIImage(named: "EmptyStates", in: .misticaCatalog, with: nil)!
-    static let badgeIcon = UIImage(named: "Notifications", in: .misticaCatalog, with: nil)!
-    static let controlsIcon = UIImage(named: "Controls", in: .misticaCatalog, with: nil)!
+    static let buttonsIcon = UIImage(named: "Buttons", in: .misticaCatalog, compatibleWith: nil)!
+    static let croutonIcon = UIImage(named: "Crouton", in: .misticaCatalog, compatibleWith: nil)!
+    static let feedbacksIcon = UIImage(named: "Feedbacks", in: .misticaCatalog, compatibleWith: nil)!
+    static let fontsIcon = UIImage(named: "Texts", in: .misticaCatalog, compatibleWith: nil)!
+    static let headerIcon = UIImage(named: "Headers", in: .misticaCatalog, compatibleWith: nil)!
+    static let inputFieldsIcon = UIImage(named: "InputFields", in: .misticaCatalog, compatibleWith: nil)!
+    static let listIcon = UIImage(named: "Lists", in: .misticaCatalog, compatibleWith: nil)!
+    static let popoverIcon = UIImage(named: "Popovers", in: .misticaCatalog, compatibleWith: nil)!
+    static let scrollContentIndicatorIcon = UIImage(named: "Popovers", in: .misticaCatalog, compatibleWith: nil)!
+    static let sectionTitleIcon = UIImage(named: "Lists", in: .misticaCatalog, compatibleWith: nil)!
+    static let segmentSelectorIcon = UIImage(named: "Tabs", in: .misticaCatalog, compatibleWith: nil)!
+    static let tagsIcon = UIImage(named: "Labels", in: .misticaCatalog, compatibleWith: nil)!
+    static let viewStatesIcon = UIImage(named: "EmptyStates", in: .misticaCatalog, compatibleWith: nil)!
+    static let badgeIcon = UIImage(named: "Notifications", in: .misticaCatalog, compatibleWith: nil)!
+    static let controlsIcon = UIImage(named: "Controls", in: .misticaCatalog, compatibleWith: nil)!
 
-    static let tabItemIcon = UIImage(named: "tabItem", in: .misticaCatalog, with: nil)!
+    static let tabItemIcon = UIImage(named: "tabItem", in: .misticaCatalog, compatibleWith: nil)!
 }

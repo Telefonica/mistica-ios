e8ff73094ebe38985fd2ab3564d8dffde4329fb1
Jose Miguel Brocal <jmbrocal@tuenti.com>
2020-09-14 18:09:56 +0200

    Delete Example app. Creates a new target in Mistica.pbxproj named MisticaApp.
    The new MisticaApp target is used generate an enterprise application of Mistica Catalog

--- MisticaApp/Source/AppDelegate.swift
+++ MisticaApp/Source/AppDelegate.swift
@@ -1,8 +1,8 @@
 //
 //  AppDelegate.swift
-//  Example
+//  MisticaApp
 //
-//  Created by Jose Miguel Brocal on 03/08/2020.
+//  Created by Jose Miguel Brocal on 14/09/2020.
 //  Copyright © 2020 Telefonica. All rights reserved.
 //
 
@@ -10,19 +10,20 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
         true
     }
 
     // MARK: UISceneSession Lifecycle
 
-    func application(_: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options _: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
-    func application(_: UIApplication, didDiscardSceneSessions _: Set<UISceneSession>) {
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
---
+++ MisticaApp/Source/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  MisticaApp
+//
+//  Created by Jose Miguel Brocal on 14/09/2020.
+//  Copyright © 2020 Telefonica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}

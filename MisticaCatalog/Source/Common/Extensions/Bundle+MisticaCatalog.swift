500bec2d921901363af175678de478c0c0dac1f8
Jose Miguel Brocal <jmbrocal@tuenti.com>
2020-09-18 17:45:25 +0200

    Revert "Use Bundle.module when Mistica is built with Swift Package Manager"
    This reverts commit 072a04cc956f11548a5bc510b6941a90dc45a887.

--- MisticaCatalog/Source/Common/Extensions/Bundle+MisticaCatalog.swift
+++ MisticaCatalog/Source/Common/Extensions/Bundle+MisticaCatalog.swift
@@ -10,11 +10,6 @@ import Foundation
 
 // A helper to access the framework bundle
 extension Bundle {
-    #if SWIFT_PACKAGE
-        static let mistica = Bundle.module
-    #else
-        private final class Internal {}
-
-        static let mistica = Bundle(for: Internal.self)
-    #endif
+    private final class Internal {}
+    static let misticaCatalog = Bundle(for: Internal.self)
 }

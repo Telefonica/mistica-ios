6be5e386f264f2cfb1e4cc29ca34ac6c75e94fd9
Pablo Bartolome <pbartolome@tuenti.com>
2020-10-29 09:56:25 +0100

    APPS-7298 Automate the release process: version bumping, generate CHANGELOG

---
+++ scripts/update-version.sh
@@ -0,0 +1,16 @@
+# Script used to change the version number on Mistica
+if [[ ! $1 =~ (0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)(-((0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*))*))?(\+([0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*))? ]]; then
+    echo "No valid version supplied"
+    exit 1
+fi
+
+VERSION_NUMBER=$1
+echo "Updating podspec, xcconfig and mistica catalog version number to $VERSION_NUMBER"
+
+# Execute from the root project folder
+cd Mistica &>/dev/null
+cd .. &>/dev/null
+
+sed -i '' -E "s/(s.version[[:space:]]+=[[:space:]]+').*(')/\1$VERSION_NUMBER\2/" Mistica.podspec
+sed -i '' -E "s/(VERSION_NUMBER = ).*/\1$VERSION_NUMBER/" Mistica/SupportFiles/Mistica.xcconfig
+sed -i '' -e '/<key>MisticaVersion<\/key>/ {' -e "n; s/<string>.*<\/string>/<string>$VERSION_NUMBER<\/string>/" -e '}' MisticaCatalog/SupportFiles/Info.plist

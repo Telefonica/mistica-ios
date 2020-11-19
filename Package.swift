d12dda75c56e3b5435e9477b404c9a306b12ec49
Jose Miguel Brocal <jmbrocal@tuenti.com>
2020-11-19 11:09:10 +0100

    APPS-7530 Fix path of MisticaTests target.

--- Package.swift
+++ Package.swift
@@ -71,7 +71,7 @@ let package = Package(
                 "Mistica",
                 "SnapshotTesting"
             ],
-            path: "Mistica/Tests",
+            path: "Mistica/Tests/MisticaTests",
             exclude: [
                 "UI/__Snapshots__"
             ]

f73b1929fa14cfca0db57db1a5baf0f2827252e5
Guille Gonzalez <guillermo.gonzalez@me.com>
2020-11-30 18:51:51 +0100

    APPS-7532 Add snapshot tests for BadgeView

---
+++ Mistica/Tests/MisticaTests/UI/BadgeTests.swift
@@ -0,0 +1,43 @@
+import Mistica
+import SnapshotTesting
+import XCTest
+
+final class BadgeTests: XCTestCase {
+    override class func setUp() {
+        super.setUp()
+
+        isRecording = false
+    }
+    
+    func testFlagBadge() {
+        assertSnapshotForAllBrands(as: .image, viewBuilder: makeFlagBadge())
+    }
+    
+    func testNumericBadge() {
+        assertSnapshotForAllBrands(as: .image, viewBuilder: makeNumericBadge())
+    }
+}
+
+private extension BadgeTests {
+    func makeFlagBadge() -> UIView {
+        let badgeView = BadgeView(style: .flag)
+        badgeView.sizeToFit()
+        
+        return badgeView
+    }
+    
+    func makeNumericBadge() -> UIView {
+        let badgesView = UIStackView()
+
+        for index in 0 ... 10 {
+            let badgeView = BadgeView(style: .numeric)
+            badgeView.value = index
+            badgesView.addArrangedSubview(badgeView)
+        }
+        
+        let size = badgesView.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
+        badgesView.frame = CGRect(origin: .zero, size: size)
+        
+        return badgesView
+    }
+}

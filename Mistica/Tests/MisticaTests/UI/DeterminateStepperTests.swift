d6bbc70e56ceae2f103833d085321852efe43dcf
jmpuerta <josemaria.puertagonzalez@telefonica.com>
2021-01-21 16:35:49 +0100

    APPS-7674 Fix updated test

--- Mistica/Tests/MisticaTests/UI/DeterminateStepperTests.swift
+++ Mistica/Tests/MisticaTests/UI/DeterminateStepperTests.swift
@@ -39,7 +39,7 @@ final class DeterminateStepperTests: XCTestCase {
             named: "assertInitialState"
         )
 
-        stepper.currentStep = 50
+        stepper.currentStep = 1
 
         assertSnapshot(
             matching: stepper,

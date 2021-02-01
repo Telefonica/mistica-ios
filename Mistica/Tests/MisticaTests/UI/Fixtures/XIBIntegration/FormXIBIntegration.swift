828a4c03988393ae3bbbf9ab1c032c2ca7c929e8
David <david.marin.calleja@gmail.com>
2021-02-01 11:25:21 +0100

    IOS-6347 Screenshot tests for FormView component (#67)
    * IOS-6347 Screenshot tests for FormView component
    
    * Run swiftformat
    
    * IOS-6347 add more behaviour and layout test to FormTest. Add new InputFieldTests test for InputField component
    
    * remove the year from the comment header
    
    * Run swiftformat
    
    Co-authored-by: dmarin <dmarin@tuenti.com>
    Co-authored-by: DavidMarinCalleja <DavidMarinCalleja@users.noreply.github.com>

---
+++ Mistica/Tests/MisticaTests/UI/Fixtures/XIBIntegration/FormXIBIntegration.swift
@@ -0,0 +1,14 @@
+//
+//  FormXIBIntegration.swift
+//
+//  Made with ❤️ by Novum
+//
+//  Copyright © Telefonica. All rights reserved.
+//
+
+import Mistica
+import UIKit
+
+class FormXIBIntegration: UIView {
+    @IBOutlet var formView: FormView!
+}

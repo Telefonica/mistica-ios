e4fc9065fd2b4dac13aa3d880fd5b1d06f31504e
Jose Miguel Brocal Gallego <jmbrocal@tuenti.com>
2021-02-16 13:29:03 +0100

    IOS-6453 Enable in-app updates in Mistica Catalog app

--- MisticaCatalog/Source/AppDelegate.swift
+++ MisticaCatalog/Source/AppDelegate.swift
@@ -6,15 +6,34 @@
 //  Copyright © Telefonica. All rights reserved.
 //
 
+import AppCenter
+import AppCenterDistribute
 import Mistica
 import UIKit
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        AppCenter.start(withAppSecret: appSecret, services: [Distribute.self])
+        
         // Configure brand style before initializing the UI
         MisticaConfig.brandStyle = .movistar
         MisticaConfig.styleControls(MisticaControlStyle.allCases)
 
         return true
+    }
+}
+
+// MARK: Private
+
+private extension AppDelegate {
+    var appSecret: String {
+        guard let object = Bundle.main.object(forInfoDictionaryKey: "APPCENTER_APP_SECRET") else {
+            fatalError("Missing key APPCENTER_APP_SECRET in Info.plist")
+        }
+        guard let string = object as? String else {
+            fatalError("Value of key APPCENTER_APP_SECRET cannot be casted to String")
+        }
+        
+        return string
     }
 }

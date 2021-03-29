9b3e84fa98e6a45ee37622170e8690b96b52b372
Julian Alonso <julian.alonsocarballo@telefonica.com>
2021-03-29 15:58:05 +0200

    IOS-6355 add snapshot tests to section title testing

---
+++ Mistica/Tests/MisticaTests/UI/SectionTitleTests.swift
@@ -0,0 +1,79 @@
+//
+//  SectionTitleTests.swift
+//  Mistica
+//
+//  Created by jalonso on 29/3/21.
+//
+
+@testable import Mistica
+import SnapshotTesting
+import XCTest
+
+final class SectionTitleTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+        UIView.setAnimationsEnabled(false)
+        
+        isRecording = false
+        MisticaConfig.brandStyle = .movistar
+    }
+    
+    func testSectionTitle() {
+        assertSnapshot(
+            matching: makeSectionTitle(title: "Section title test"),
+            as: .image(on: .iPhoneSe)
+        )
+    }
+}
+
+private extension SectionTitleTests {
+    func makeSectionTitle(title: String) -> UIViewController {
+        SectionTitleViewControler(sectionTitle: title)
+    }
+}
+
+private class SectionTitleViewControler: UITableViewController {
+    
+    private var sectionTitle: String!
+    
+    init(sectionTitle: String) {
+        self.sectionTitle = sectionTitle
+        super.init(style: .grouped)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.separatorStyle = .none
+        tableView.sectionHeaderHeight = UITableView.automaticDimension
+        tableView.tableFooterView = UIView(frame: CGRect.zero)
+        tableView.sectionFooterHeight = 0.0
+
+        tableView.register(SectionTitleHeaderView.self, forHeaderFooterViewReuseIdentifier: "Header")
+    }
+
+    override func numberOfSections(in _: UITableView) -> Int {
+        1
+    }
+
+    override func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
+        0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: "Header") as! SectionTitleHeaderView
+        headerView.title = sectionTitle
+
+        return headerView
+    }
+
+}

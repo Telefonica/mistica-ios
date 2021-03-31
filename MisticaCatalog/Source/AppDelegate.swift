7ef77b7c9c16651b0036d8f52b8a26f22f3bbfec
jmbrocal <jmbrocal@users.noreply.github.com>
2021-03-31 16:27:06 +0200

    Run swiftformat

--- MisticaCatalog/Source/AppDelegate.swift
+++ MisticaCatalog/Source/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
         // Configure In-App updates
         AppCenter.start(withAppSecret: "bfef11d4-cda0-4d89-806e-4014c2d80feb", services: [Distribute.self])
-        
+
         // Configure brand style before initializing the UI
         MisticaConfig.brandStyle = .movistar
         MisticaConfig.styleControls(MisticaControlStyle.allCases)

2a77a214e9a8ab42f6877024c99cbbaacca0e3a0
José Miguel Brocal Gallego <jmbrocal@tuenti.com>
2021-04-05 10:28:06 +0200

    chore(MisticaCatalog): Activate in-app updates of Mistica Catalog app (#102)

--- MisticaCatalog/Source/AppDelegate.swift
+++ MisticaCatalog/Source/AppDelegate.swift
@@ -6,11 +6,16 @@
 //  Copyright © Telefonica. All rights reserved.
 //
 
+import AppCenter
+import AppCenterDistribute
 import Mistica
 import UIKit
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        // Configure In-App updates
+        AppCenter.start(withAppSecret: "bfef11d4-cda0-4d89-806e-4014c2d80feb", services: [Distribute.self])
+
         // Configure brand style before initializing the UI
         MisticaConfig.brandStyle = .movistar
         MisticaConfig.styleControls(MisticaControlStyle.allCases)

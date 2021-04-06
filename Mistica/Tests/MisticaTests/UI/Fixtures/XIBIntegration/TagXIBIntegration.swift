6867a8714c6155cb1edaa46caabf46bd934c3780
Julian Alonso <julian.alonsocarballo@telefonica.com>
2021-04-06 12:34:39 +0200

    IOS-6311 add snapshot test to TagView

---
+++ Mistica/Tests/MisticaTests/UI/Fixtures/XIBIntegration/TagXIBIntegration.swift
@@ -0,0 +1,13 @@
+//
+//  TagXIBIntegration.swift
+//  MisticaTests
+//
+//  Created by jalonso on 6/4/21.
+//
+
+import Mistica
+import UIKit
+
+class TagXIBIntegration: UIView {
+    @IBOutlet var tagView: TagView!
+}
---
+++ Mistica/Tests/MisticaTests/UI/TagTests.swift
@@ -0,0 +1,47 @@
+//
+//  TagTests.swift
+//
+//  Made with ❤️ by Novum
+//
+//  Copyright © Telefonica. All rights reserved.
+//
+
+@testable import Mistica
+import SnapshotTesting
+import XCTest
+
+final class TagTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+        UIView.setAnimationsEnabled(false)
+
+        isRecording = false
+    }
+    
+    func testTagView() {
+        assertSnapshotForAllBrands(
+            as: .image(size: CGSize(width: 100.0, height: 33.0)),
+            viewBuilder: makeTag(text: "Tag")
+        )
+    }
+    
+    func testTagXibIntegration() {
+        MisticaConfig.brandStyle = .movistar
+
+        let view = TagXIBIntegration.viewFromNib()
+        view.tagView.text = "Xib integration"
+
+        assertSnapshot(
+            matching: view,
+            as: .image
+        )
+    }
+}
+
+private extension TagTests {
+    func makeTag(text: String) -> TagView {
+        let view = TagView()
+        view.text = text
+        return view
+    }
+}

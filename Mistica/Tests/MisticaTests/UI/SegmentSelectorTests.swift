97d3406fe309c39887e86d6eaec9af73ffa17b07
jmpuerta <josemaria.puertagonzalez@telefonica.com>
2021-08-31 16:21:13 +0200

    fix(SegmentSelector): Fix segment selection after theme changes

---
+++ Mistica/Tests/MisticaTests/UI/SegmentSelectorTests.swift
@@ -0,0 +1,42 @@
+//
+//  SegmentSelectorTests.swift
+//  
+//
+//  Created by jmpuerta on 31/8/21.
+//
+
+@testable import Mistica
+import SnapshotTesting
+import XCTest
+
+final class SegmentSelectorTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+        UIView.setAnimationsEnabled(false)
+
+        isRecording = false
+    }
+    
+    func testSegmentsInSegmentSelector() {
+        assertSnapshotForAllBrandsAndStyles(
+            as: .image(size: CGSize(width: 320, height: 49)),
+            viewBuilder: makeSegmentSelector(segments: 5, hasSelectedItem: false)
+        )
+    }
+    
+    func testSelectedSegmentsInSegmentSelector() {
+        assertSnapshotForAllBrandsAndStyles(
+            as: .image(size: CGSize(width: 320, height: 49)),
+            viewBuilder: makeSegmentSelector(segments: 5, hasSelectedItem: true)
+        )
+    }
+    
+    func makeSegmentSelector(segments: Int, hasSelectedItem: Bool) -> SegmentSelector {
+        let segments = (0..<segments).map { Segment(id: "\($0)", title: "Segment")}
+        let segmentSelector = SegmentSelector(segments: segments)
+        if hasSelectedItem, let segment = segments.first {
+            segmentSelector.select(segment)
+        }
+        return segmentSelector
+    }
+}

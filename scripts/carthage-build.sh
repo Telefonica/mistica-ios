0ddf98165b3498c63eac95d68136361587fe6fee
Jose Miguel Brocal Gallego <jmbrocal@tuenti.com>
2021-10-18 17:14:25 +0200

    IOS-7081 Support Xcode 13 and Swift 5.5

--- scripts/carthage-build.sh
+++ scripts/carthage-build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-# This script is used to build carthage dependencies with Xcode 12. 
+# This script is used to update and buld carthage dependencies 
 
 build()
 {
@@ -12,20 +12,14 @@ build()
 	# Create a new temporal config file
 	: $(mktemp $xcconfig)
 
-	# Force module stability
-    echo "BUILD_LIBRARY_FOR_DISTRIBUTION = YES" >> $xcconfig
-
-	# Xcode 12 workaround
-	echo 'EXCLUDED_ARCHS__EFFECTIVE_PLATFORM_SUFFIX_simulator__NATIVE_ARCH_64_BIT_x86_64=arm64 arm64e armv7 armv7s armv6 armv8' >> $xcconfig
-	echo 'EXCLUDED_ARCHS=$(inherited) $(EXCLUDED_ARCHS__EFFECTIVE_PLATFORM_SUFFIX_$(EFFECTIVE_PLATFORM_SUFFIX)__NATIVE_ARCH_64_BIT_$(NATIVE_ARCH_64_BIT))' >> $xcconfig
-
-	export XCODE_XCCONFIG_FILE="$xcconfig"
-
-	carthage build --platform iOS --no-use-binaries --cache-builds
+	# Reduce binary size by building only archs supported by the Novum client 
+    echo "ARCHS = arm64 x86_64" >> $xcconfig 
+    export XCODE_XCCONFIG_FILE="$xcconfig" 
+    carthage update --platform iOS --use-xcframeworks $carthage_args
 }
 
 # Update dependency without building
 carthage update --no-build
 
-# Build dependency with extra flags to prevent issues in Xcode 12
+# Build dependencies
 build

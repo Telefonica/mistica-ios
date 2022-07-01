5d16b3b28fe58a60297903b81a9c8476efd48a14
José Manuel Rodríguez Moreno <josemanuel.rodriguezmoreno@telefonica.com>
2022-07-01 16:24:42 +0200

    IOS-7154 Changed textPreset4 light for textPreset3 regular in Empty State description.

--- Mistica/Source/Components/EmptyState/Internals/EmptyStateMessagesContent.swift
+++ Mistica/Source/Components/EmptyState/Internals/EmptyStateMessagesContent.swift
@@ -99,7 +99,7 @@ extension EmptyStateMessagesContent {
         let descriptionSpacing: CGFloat = isCard ? Constants.descriptionTopSpacingCard : Constants.descriptionTopSpacingDefault
         descriptionTitleTopSpacing = descriptionSpacing
         titleLabel.font = isCard ? .textPreset4(weight: .regular) : .textPreset6(weight: .light)
-        descriptionLabel.font = isCard ? .textPreset2(weight: .regular) : .textPreset4(weight: .light)
+        descriptionLabel.font = isCard ? .textPreset2(weight: .regular) : .textPreset3(weight: .regular)
     }
 }
 
@@ -126,7 +126,7 @@ private extension EmptyStateMessagesContent {
         titleLabel.numberOfLines = 0
         titleLabel.topSpacing = 24
 
-        descriptionLabel.font = .textPreset4(weight: .light)
+        descriptionLabel.font = .textPreset3(weight: .regular)
         descriptionLabel.textColor = .textSecondary
         descriptionLabel.minHeight = 18
         descriptionLabel.numberOfLines = 0

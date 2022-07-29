d07049d7301f6dba0c18b9f26f1dc6c405b236cd
jmpuerta <josemaria.puertagonzalez@telefonica.com>
2022-07-29 09:47:43 +0200

    Add init to DefaultMisticaBrandAssets

--- Sources/MisticaCommon/Assets/DefaultMisticaBrandAssets.swift
+++ Sources/MisticaCommon/Assets/DefaultMisticaBrandAssets.swift
@@ -9,52 +9,56 @@
 import Foundation
 import SwiftUI
 
-struct DefaultMisticaBrandAssets: MisticaBrandAssets {
-    var arrowRight: UIImage {
+public struct DefaultMisticaBrandAssets: MisticaBrandAssets {
+    public init() {
+        // Do nothing
+    }
+    
+    public var arrowRight: UIImage {
         UIImage(named: "icn_arrow_right", type: .common)!
     }
 
-    var checkmarkIcon: UIImage {
+    public var checkmarkIcon: UIImage {
         UIImage(named: "icn_checkbox_check", type: .common)!
     }
 
-    var eyeEnabled: UIImage {
+    public var eyeEnabled: UIImage {
         UIImage(named: "icn_eye_enabled", type: .common)!
     }
 
-    var eyeDisabled: UIImage {
+    public var eyeDisabled: UIImage {
         UIImage(named: "icn_eye_disabled", type: .common)!
     }
 
-    var arrowDown: UIImage {
+    public var arrowDown: UIImage {
         UIImage(named: "icn_dropmenu", type: .common)!
     }
 
-    var search: UIImage {
+    public var search: UIImage {
         UIImage(named: "icn_search", type: .common)!
     }
 
-    var calendar: UIImage {
+    public var calendar: UIImage {
         UIImage(named: "icn_calendar", type: .common)!
     }
 
-    var iconNotificationInfo: UIImage? {
+    public var iconNotificationInfo: UIImage? {
         UIImage(named: "iconNotificationInfo", type: .branded)
     }
 
-    var closeButtonBlackSmallIcon: UIImage {
+    public var closeButtonBlackSmallIcon: UIImage {
         UIImage(named: "icn_close_black_small", type: .common)!
     }
 
-    var checkAnimation: NSDataAsset? {
+    public var checkAnimation: NSDataAsset? {
         NSDataAsset(named: "StepperSuccess", type: .branded)
     }
 
-    var successAnimation: NSDataAsset? {
+    public var successAnimation: NSDataAsset? {
         NSDataAsset(named: "FeedbackSuccess", type: .branded)
     }
 
-    var errorAnimation: NSDataAsset? {
+    public var errorAnimation: NSDataAsset? {
         NSDataAsset(named: "FeedbackError", type: .branded)
     }
 }

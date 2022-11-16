67718476f39243d4971f91c7ffbd77c8ecf08c5d
jmbrocal <jmbrocal@users.noreply.github.com>
2022-11-16 10:52:00 +0100

    Run swiftformat

--- Sources/Mistica/Utils/Extensions/UIImageView+URL.swift
+++ Sources/Mistica/Utils/Extensions/UIImageView+URL.swift
@@ -28,7 +28,7 @@ extension UIImageView {
                 urlForDarkMode: urlForDarkMode,
                 completion: { [weak self] lightImageData, darkImageData in
                     guard let lightImageData else { return }
-                    
+
                     DispatchQueue.main.async {
                         self?.updateImage(imageData: lightImageData, darkImageData: darkImageData)
                     }

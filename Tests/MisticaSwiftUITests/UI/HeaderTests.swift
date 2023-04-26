0dffdeb12f261ef6b60bb556a8a593d6dda413dc
DavidMarinCalleja <DavidMarinCalleja@users.noreply.github.com>
2023-04-26 13:39:23 +0200

    Run swiftformat

--- Tests/MisticaSwiftUITests/UI/HeaderTests.swift
+++ Tests/MisticaSwiftUITests/UI/HeaderTests.swift
@@ -28,67 +28,67 @@ extension HeaderTests {
             pretitle: "The pretitle",
             style: .normal
         )
-        
+
         assertSnapshot(
             matching: UIHostingController(rootView: header),
             as: .image(on: .iPhone8)
         )
     }
-    
+
     func testMinimalPretitleHeaderWithInverseStyle() {
         let header = Header(
             pretitle: "The pretitle",
             style: .inverse
         )
-        
+
         assertSnapshot(
             matching: UIHostingController(rootView: header),
             as: .image(on: .iPhone8)
         )
     }
-    
+
     func testMinimalTitleHeader() {
         let header = Header(
             title: "The title",
             style: .normal
         )
-        
+
         assertSnapshot(
             matching: UIHostingController(rootView: header),
             as: .image(on: .iPhone8)
         )
     }
-    
+
     func testMinimalTitleHeaderWithInverseStyle() {
         let header = Header(
             title: "The title",
             style: .inverse
         )
-        
+
         assertSnapshot(
             matching: UIHostingController(rootView: header),
             as: .image(on: .iPhone8)
         )
     }
-    
+
     func testMinimalDescriptionHeader() {
         let header = Header(
             description: "The description",
             style: .normal
         )
-        
+
         assertSnapshot(
             matching: UIHostingController(rootView: header),
             as: .image(on: .iPhone8)
         )
     }
-    
+
     func testMinimalDescriptionHeaderWithInverseStyle() {
         let header = Header(
             description: "The description",
             style: .inverse
         )
-        
+
         assertSnapshot(
             matching: UIHostingController(rootView: header),
             as: .image(on: .iPhone8)
@@ -104,13 +104,13 @@ extension HeaderTests {
             description: "The description",
             style: .normal
         )
-        
+
         assertSnapshot(
             matching: UIHostingController(rootView: header),
             as: .image(on: .iPhone8)
         )
     }
-    
+
     func testFullHeaderWithInverseStyle() {
         let header = Header(
             pretitle: "The pretitle",
@@ -118,13 +118,13 @@ extension HeaderTests {
             description: "The description",
             style: .inverse
         )
-        
+
         assertSnapshot(
             matching: UIHostingController(rootView: header),
             as: .image(on: .iPhone8)
         )
     }
-    
+
     func testFullHeaderWithLongTexts() {
         let header = Header(
             pretitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
@@ -132,13 +132,13 @@ extension HeaderTests {
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
             style: .normal
         )
-        
+
         assertSnapshot(
             matching: UIHostingController(rootView: header),
             as: .image(on: .iPhone8)
         )
     }
-    
+
     func testFullHeaderWithLongTextsAndInverseStyle() {
         let header = Header(
             pretitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
@@ -146,11 +146,10 @@ extension HeaderTests {
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
             style: .inverse
         )
-        
+
         assertSnapshot(
             matching: UIHostingController(rootView: header),
             as: .image(on: .iPhone8)
         )
-
     }
 }

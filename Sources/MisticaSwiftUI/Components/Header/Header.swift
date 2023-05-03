f6c56237bf7d15d1e1e7aa2196590c689aa7d487
David Marín <david.marin.calleja@telefonica.com>
2023-05-03 11:06:07 +0200

    IOS-8008 Extract the HeaderText class

--- Sources/MisticaSwiftUI/Components/Header/Header.swift
+++ Sources/MisticaSwiftUI/Components/Header/Header.swift
@@ -8,25 +8,6 @@
 
 import SwiftUI
 
-public struct HeaderText {
-    let text: String
-    let lineLimit: Int
-    let accessibilityLabel: String?
-    let accessibilityIdentifier: String?
-
-    public init(
-        text: String,
-        lineLimit: Int = 0,
-        accessibilityLabel: String? = nil,
-        accessibilityIdentifier: String? = nil
-    ) {
-        self.text = text
-        self.lineLimit = lineLimit
-        self.accessibilityLabel = accessibilityLabel
-        self.accessibilityIdentifier = accessibilityIdentifier
-    }
-}
-
 public struct Header: View {
     public enum HeaderStyle {
         case normal

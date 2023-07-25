aac8318d55d1818b91119288c9323af504a6c083
Carlos Martínez Medina <carlos.martinezmedina@telefonica.com>
2023-07-25 17:33:49 +0200

    feat(BottomSheet): Change load and derived functions

--- Sources/Mistica/Utils/Extensions/UIImageView+URL.swift
+++ Sources/Mistica/Utils/Extensions/UIImageView+URL.swift
@@ -6,72 +6,24 @@
 //  Copyright © Telefonica. All rights reserved.
 //
 
-import SDWebImageSVGNativeCoder
 import UIKit
+import SDWebImage
+import SDWebImageSVGNativeCoder
 
 extension UIImageView {
     /// Loads the urls asynchronously
     func load(url: URL, urlForDarkMode: URL? = nil, renderAsTemplate: Bool = false) {
-        guard UIView.areAnimationsEnabled else {
-            downloadImage(
-                url: url,
-                urlForDarkMode: urlForDarkMode
-            ) { [weak self] lightImage, darkImage in
-                guard let lightImage else { return }
-                self?.updateImage(image: lightImage, darkImage: darkImage)
-            }
-            return
-        }
+        let coder = SDImageSVGNativeCoder.shared
+        SDImageCodersManager.shared.removeCoder(coder)
+        SDImageCodersManager.shared.addCoder(coder)
 
-        DispatchQueue.global().async { [weak self] in
-            self?.downloadImage(
-                url: url,
-                urlForDarkMode: urlForDarkMode,
-                completion: { [weak self] lightImage, darkImage in
-                    guard let lightImage else { return }
-
-                    DispatchQueue.main.async {
-                        self?.updateImage(image: lightImage, darkImage: darkImage)
-                    }
+        sd_setImage(with: url) { [weak self] lightImage, _, _, _  in
+            self?.sd_setImage(with: urlForDarkMode) { darkImage, _, _, _  in
+                if let lightImage, let darkImage {
+                    lightImage.imageAsset?.register(darkImage, with: UITraitCollection(userInterfaceStyle: .dark))
+                    self?.image = lightImage
                 }
-            )
+            }
         }
-    }
-}
-
-private extension UIImageView {
-    func downloadImage(url: URL, urlForDarkMode: URL?, completion: @escaping (UIImage?, UIImage?) -> Void) {
-        var light: UIImage?, dark: UIImage?
-        if let data = try? Data(contentsOf: url) {
-            light = decode(data: data)
-        }
-
-        if let url = urlForDarkMode, let data = try? Data(contentsOf: url) {
-            dark = decode(data: data)
-        }
-
-        completion(light, dark)
-    }
-
-    func decode(data: Data) -> UIImage? {
-        if let image = UIImage(data: data) {
-            return image
-        } else if let image = SDImageSVGNativeCoder.shared.decodedImage(with: data) {
-            return image
-        } else {
-            return nil
-        }
-    }
-
-    func updateImage(image: UIImage, darkImage: UIImage?, renderAsTemplate: Bool = false) {
-        let renderMode = renderAsTemplate ? UIImage.RenderingMode.alwaysTemplate : .automatic
-        let lightImage = image.withRenderingMode(renderMode)
-        let darkImage = darkImage?.withRenderingMode(renderMode)
-
-        if let darkImage {
-            lightImage.imageAsset?.register(darkImage, with: .init(userInterfaceStyle: .dark))
-        }
-
-        self.image = lightImage
     }
 }

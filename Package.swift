b19973c8c4e125b347cd7b5570eae171a0f7114a
Carlos Martinez Medina <carlos.martinezmedina@telefonica.com>
2023-07-25 13:09:07 +0200

    feat(BottomSheet): Update Package.swift
    Co-authored-by: Alejandro Megías <Alejandro.Megias.Zapata@telefonica.com>

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/airbnb/lottie-ios.git", exact: "3.1.8"),
         .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", exact: "1.8.2"),
-        .package(url: "https://github.com/SDWebImage/SDWebImageSVGNativeCoder.git", from: "0.1.1")
+        .package(url: "https://github.com/SDWebImage/SDWebImageSVGNativeCoder.git", exact: "0.1.1")
     ],
     targets: [
         .target(

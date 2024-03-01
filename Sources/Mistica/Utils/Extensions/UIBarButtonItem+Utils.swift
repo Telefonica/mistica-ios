c25f428699c951d27924467600c9b0aa458a70fb
Alejandro Ruiz Ponce <alejandro.ruizponce@telefonica.com>
2024-03-01 13:32:26 +0100

    Add extension UIButton for SVG support load

--- Sources/Mistica/Utils/Extensions/UIBarButtonItem+Utils.swift
+++ Sources/Mistica/Utils/Extensions/UIBarButtonItem+Utils.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import SDWebImage
+import SDWebImageSVGCoder
 
 extension UIBarButtonItem {
     convenience init(icon _: UIImage,
@@ -45,5 +47,29 @@ extension UIButton {
         titleLabel?.font = .textPreset3(weight: .regular)
         sizeToFit()
         frameHeight = 36.0
+    }
+
+    func setImageFromURL(url: URL, urlForDarkMode: URL? = nil) {
+        let coder = SDImageSVGCoder.shared
+        SDImageCodersManager.shared.removeCoder(coder)
+        SDImageCodersManager.shared.addCoder(coder)
+
+        let imageAsset = UIImageAsset()
+
+        sd_setImage(with: url, for: .normal) { lightResult, _, _, _ in
+            guard let lightImage = lightResult else { return }
+            imageAsset.register(lightImage, with: .init(userInterfaceStyle: .light))
+
+            if let darkURL = urlForDarkMode {
+                self.sd_setImage(with: darkURL, for: .normal) { darkResult, _, _, _ in
+                    if let darkImage = darkResult {
+                        imageAsset.register(darkImage, with: .init(userInterfaceStyle: .dark))
+                        self.setImage(imageAsset.image(with: self.traitCollection), for: .normal)
+                    }
+                }
+            } else {
+                self.setImage(imageAsset.image(with: self.traitCollection), for: .normal)
+            }
+        }
     }
 }

fb6f700dedff94fe34e98e19ec42d0bca67d5619
Alejandro Ruiz Ponce <alejandro.ruizponce@telefonica.com>
2024-04-03 12:21:46 +0200

    Add description for lottie extension

--- Sources/MisticaCommon/Extensions/Lottie+Utils.swift
+++ Sources/MisticaCommon/Extensions/Lottie+Utils.swift
@@ -9,6 +9,11 @@
 import Foundation
 import Lottie
 
+/**
+  This extension is a fix has been necessary because the animations in  snapshot tests were being cut off without rendering correctly and as a consequence many tests were failing. This is due to a bug that exists in        CoreAnimation rendering engine when rendering in snapshots without mainThread mode, you can see more information here:
+  https://github.com/airbnb/lottie-ios/discussions/1769#discussioncomment-3860672
+ **/
+
 public extension Lottie.LottieConfiguration {
     static var current: LottieConfiguration = {
         var configuration: LottieConfiguration = .init(renderingEngine: .automatic)

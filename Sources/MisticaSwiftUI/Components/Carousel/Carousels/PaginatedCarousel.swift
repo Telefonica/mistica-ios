a39c5b9be010c8fe7c88306482b9a2a15174ad15
Francisco Rodriguez <franciscojose.rodriguezperez@telefonica.com>
2024-04-04 12:15:00 +0200

    Remove unintended change

--- Sources/MisticaSwiftUI/Components/Carousel/Carousels/PaginatedCarousel.swift
+++ Sources/MisticaSwiftUI/Components/Carousel/Carousels/PaginatedCarousel.swift
@@ -41,7 +41,7 @@ struct PaginatedCarousel<Element, Content: View>: View {
                 content
             }
             .content
-            .offset(x: xOffset(at: index))
+            .offset(x: xOffset())
             .frame(width: geometry.size.width, alignment: .leading)
             .gesture(dragGesture(geometry: geometry))
             .animation(.misticaTimingCurve, value: contentOffset)

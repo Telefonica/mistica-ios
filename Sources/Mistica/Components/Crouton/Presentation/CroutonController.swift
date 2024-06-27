21bf939a74f42bd57230359179a85fda86a1cc34
David Hidalgo Fadrique <david.hidalgofadrique@telefonica.com>
2024-06-27 08:56:44 +0200

    feat(Accesibility): Accesibility of Snackbars and croutons (#375)
    * Added accessibility helper and post snackbar/croutons accessibility notifications
    
    * Set announcement speech queue to false
    
    * Restored queue speech to true
    
    * Test screen changed
    
    * Test using layout changed
    
    * Back to post accessibility announcement
    
    * Set speech queue announcement to false
    
    * Test iOS 17 new acnnouncements functionality
    
    * Test with low priority
    
    * Added pre iOS17 fallback for accessibility announcement
    
    * Added accessibility label for snackbar/crouton action config
    
    * Run swiftformat
    
    * Test low priority
    
    * Undo low priority test
    
    * Added comments to accessibility post method
    Changed iOS16- accessibilitySpeechQueueAnnouncement to false
    
    * Fixed catalog missing accessibility action config parameter
    
    ---------
    
    Co-authored-by: dhidalgofadrique <dhidalgofadrique@users.noreply.github.com>

--- Sources/Mistica/Components/Crouton/Presentation/CroutonController.swift
+++ Sources/Mistica/Components/Crouton/Presentation/CroutonController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 public class CroutonController: NSObject {
     public typealias Token = UUID
-    public typealias ActionConfig = (text: String, handler: DidTapActionBlock)
+    public typealias ActionConfig = (text: String, accessibilityLabel: String?, handler: DidTapActionBlock)
     fileprivate typealias OngoingCrouton = (token: Token, croutonView: CroutonView, rootViewController: () -> UIViewController?)
 
     public typealias DismissHandlerBlock = (SnackbarDismissReason) -> Void
@@ -73,10 +73,14 @@ public extension CroutonController {
         }
 
         let overwrittenAction = config.dismissInterval.action.map { action -> ActionConfig in
-            (action.title, {
-                dismissHandler(.button)
-                action.handler()
-            })
+            (
+                action.title,
+                action.accessibilityTitleLabel,
+                {
+                    dismissHandler(.button)
+                    action.handler()
+                }
+            )
         }
 
         let token = Token()

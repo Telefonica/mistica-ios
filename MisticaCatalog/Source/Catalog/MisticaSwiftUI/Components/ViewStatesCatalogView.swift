d01edbf9c1ca677765b7a7eeda4208f576c93397
Alejandro Ruiz Ponce <alejandro.ruizponce@telefonica.com>
2024-12-16 14:16:48 +0100

    Rebase with main

---
+++ MisticaCatalog/Source/Catalog/MisticaSwiftUI/Components/ViewStatesCatalogView.swift
@@ -0,0 +1,99 @@
+//
+//  ViewStatesCatalogView.swift
+//  MisticaCatalog
+//
+//  Created by Alejandro Ruiz on 27/11/24.
+//
+
+
+import SwiftUI
+import MisticaSwiftUI
+
+struct ViewStatesCatalogView: View {
+    private let viewStateTypes: [ViewStateType] = [.loadError]
+    @State var selectedTypeIndex = 0
+    @State private var titleText: String = "Oops, something went wrong"
+    @State private var descriptionText: String = "There was an error loading the content, please, check your network connection and try again later."
+    @State private var actionButtonTitle: String = "Retry"
+    @State private var showActionButton: Bool = true
+    @State private var showState: Bool = false
+
+    var body: some View {
+        ZStack {
+            if !showState {
+                List {
+                    section("View State Type") {
+                        viewStateTypePicker
+                    }
+                    
+                    section("Title (Optional)") {
+                        TextField("Title (Optional)", text: $titleText)
+                    }
+                    
+                    section("Description") {
+                        TextField("Description", text: $descriptionText)
+                    }
+                    
+                    section("Action Button") {
+                        TextField("Action Button Title", text: $actionButtonTitle)
+                        Toggle("Show Action Button", isOn: $showActionButton)
+                    }
+                    
+                    Button("Show State") {
+                        showState = true
+                    }
+                    .buttonStyle(.misticaPrimary())
+                }
+            } else {
+                LoadErrorView(
+                    titleText: titleText.isEmpty ? nil : titleText,
+                    descriptionText: descriptionText.isEmpty ? "Description text is mandatory" : descriptionText,
+                    showActionButton: showActionButton,
+                    actionButtonTitle: actionButtonTitle,
+                    onRetry: {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                            showState = false
+                        }
+                    },
+                    onAppear: {
+                        if !showActionButton {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                                showState = false
+                            }
+                        }
+                    }
+                )
+            }
+        }
+        .navigationBarTitle("View States")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+    
+    @ViewBuilder
+    var viewStateTypePicker: some View {
+        picker($selectedTypeIndex, options: viewStateTypes)
+    }
+}
+
+enum ViewStateType {
+    case loadError
+}
+
+extension ViewStateType: Swift.CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .loadError:
+            return "Load error"
+        }
+    }
+}
+
+#if DEBUG
+struct UICatalogViewStatesView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            ViewStatesCatalogView()
+        }
+    }
+}
+#endif

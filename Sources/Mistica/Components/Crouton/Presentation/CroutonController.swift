827964b3f4b4114dd75d3c40c01899472e3bae95
José Manuel Rodríguez Moreno <josemanuel.rodriguezmoreno@telefonica.com>
2025-03-17 12:35:22 +0100

    feat(Crouton): IOS-11139 Remove crouton queue to avoid enqueuing messages. From now … (#437)
    BREAKING CHANGE: UIKit Crouton no longer allows message queuing. If a new crouton is presented, the previous one will be discarded. showCrouton and dismiss CroutonController methods have been modified to remove the token we used to dismiss a specific crouton.
    
    * IOS-11139 Remove crouton queue to avoid enqueuing messages. From now if a new crouton is launched, the previous one will be removed.
    
    * Run swiftformat
    
    ---------
    
    Co-authored-by: idenjoe <idenjoe@users.noreply.github.com>

--- Sources/Mistica/Components/Crouton/Presentation/CroutonController.swift
+++ Sources/Mistica/Components/Crouton/Presentation/CroutonController.swift
@@ -14,14 +14,11 @@ public class CroutonController: NSObject {
         public static let `default`: Closure = { UIApplication.shared.windows.filter(\.isKeyWindow).first?.rootViewController }
     }
 
-    public typealias Token = UUID
     public typealias ActionConfig = (text: String, accessibilityLabel: String?, handler: DidTapActionBlock)
-
     public typealias DismissHandlerBlock = (SnackbarDismissReason) -> Void
     public typealias DidTapActionBlock = () -> Void
 
-    private var croutonViewList = [OngoingCrouton]()
-    private var showingToken: Token?
+    private var currentCroutonView: CroutonView?
 
     public static let shared = CroutonController()
 
@@ -31,7 +28,7 @@ public class CroutonController: NSObject {
 // MARK: Public functions
 
 public extension CroutonController {
-    /// Show a crouton (or enqueue one if there is already a crouton shown)
+    /// Show a crouton
     /// - Parameters:
     ///   - text: The text to display in the crouton
     ///   - action: An optional action which will show a button with the given title and invoke the handler when the button is pressed
@@ -39,14 +36,13 @@ public extension CroutonController {
     ///   - dismissHandler: A handler which is called when the handler is removed from the screen
     ///   - exactViewController: The exacti viewCotroller where the croutono will be show. Has priority over rootViewController
     ///   - rootViewController: The root view controller that will show the crouton.
-    @discardableResult
     func showCrouton(
         config: SnackbarConfig,
         style: CroutonStyle = .info,
         dismissHandler: DismissHandlerBlock? = nil,
         exactViewController: UIViewController? = nil,
         rootViewController: RootViewController.Closure? = nil
-    ) -> Token {
+    ) {
         assertMainThread()
 
         let styleConfig = CroutonConfig(style: style, croutonDismissInterval: config.dismissInterval)
@@ -68,7 +64,6 @@ public extension CroutonController {
             )
         }
 
-        let token = Token()
         let croutonView = CroutonView(
             text: config.title,
             action: overwrittenAction,
@@ -78,38 +73,22 @@ public extension CroutonController {
         )
 
         let ongoingCrouton = OngoingCrouton(
-            token: token,
             croutonView: croutonView,
             exactViewController: exactViewController,
             rootViewController: rootViewController
         )
-        show(ongoingCrouton)
 
-        return token
+        show(ongoingCrouton)
     }
 
     var isShowingACrouton: Bool {
-        showingToken != nil
+        currentCroutonView != nil
     }
 
-    /// Dismisses (or removes from enqueued croutons) the crouton identified by `token`
-    /// - Parameter token: a unique token that identifies a crouton
-    func dismiss(token: Token) {
+    /// Dismisses the current crouton if it exists
+    func dismiss() {
         assertMainThread()
-
-        // There has to be a shown crouton (even if it's not the one we are interested in)
-        guard let showingToken = showingToken else { return }
-
-        if showingToken == token {
-            dismissCurrentCrouton()
-        } else if let index = croutonViewList.firstIndex(where: { token == $0.token }) {
-            croutonViewList.remove(at: index)
-        }
-    }
-
-    func dismissAll() {
-        assertMainThread()
-        dismissAllFromCurrentCrouton()
+        dismissCurrentCrouton()
     }
 }
 
@@ -117,48 +96,14 @@ public extension CroutonController {
 
 private extension CroutonController {
     func show(_ crouton: OngoingCrouton) {
-        enqueue(crouton)
-        showEnqueuedCrouton()
+        dismissCurrentCrouton()
+        guard let view = crouton.view() else { return }
+        crouton.croutonView.show(in: view)
+        currentCroutonView = crouton.croutonView
     }
 
     func dismissCurrentCrouton() {
-        guard let crouton = dequeue() else { return }
-
-        crouton.croutonView.dismiss {
-            self.showingToken = nil
-            self.showEnqueuedCrouton()
-        }
-    }
-
-    func dismissAllFromCurrentCrouton() {
-        guard let ongoingCrouton = dequeue() else { return }
-
-        ongoingCrouton.croutonView.dismiss {
-            self.showingToken = nil
-
-            while let ongoingCrouton = self.dequeue() {
-                ongoingCrouton.croutonView.dismiss()
-            }
-        }
-    }
-
-    func enqueue(_ ongoingCrouton: OngoingCrouton) {
-        croutonViewList.append(ongoingCrouton)
-    }
-
-    func dequeue() -> OngoingCrouton? {
-        guard !croutonViewList.isEmpty else { return nil }
-
-        return croutonViewList.remove(at: 0)
-    }
-
-    func showEnqueuedCrouton() {
-        guard showingToken == nil else { return }
-        guard let ongoingCrouton = croutonViewList.first else { return }
-        guard let containerView = ongoingCrouton.view() else { return }
-
-        showingToken = ongoingCrouton.token
-
-        ongoingCrouton.croutonView.show(in: containerView)
+        currentCroutonView?.dismiss()
+        currentCroutonView = nil
     }
 }

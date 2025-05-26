a97def4c4822fe8610d7a1bcc519d3c5cee0c85d
David Hidalgo <david.hidalgofadrique@telefonica.com>
2025-05-26 15:05:41 +0200

    IOS-11218: Assign theme variant as enum value (with a "." before)

--- scripts/mistica-skin-generator/generators/theme-variants/brand-theme-variants-generator.js
+++ scripts/mistica-skin-generator/generators/theme-variants/brand-theme-variants-generator.js
@@ -5,7 +5,7 @@ const template = (prefix, themeVariants) => `
 import Foundation
 
 struct ${prefix}ThemeVariants: MisticaThemeVariants {
-${themeVariants.map(([name, themeVariant]) => `    var ${name}: MisticaThemeVariantType = ${themeVariant.value}`).join('\n')}
+${themeVariants.map(([name, themeVariant]) => `    var ${name}: MisticaThemeVariantType = .${themeVariant.value}`).join('\n')}
 }
 `;
 

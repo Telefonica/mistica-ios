c537aa5ce4ea486492d400bc4515c69a286be1e2
Alejandro Megías <Alejandro.Megias.Zapata@telefonica.com>
2025-09-30 10:19:13 +0200

    Update current skins

--- Sources/MisticaCommon/Radius/Brands/TuCornerRadius.swift
+++ Sources/MisticaCommon/Radius/Brands/TuCornerRadius.swift
@@ -11,10 +11,12 @@ import Foundation
 struct TuCornerRadius: MisticaCornerRadius {
     var avatar: CGFloat = MisticaRadiusConstants.roundedRadius
     var bar: CGFloat = MisticaRadiusConstants.roundedRadius
-    var button: CGFloat = MisticaRadiusConstants.roundedRadius
+    var button: CGFloat = 32
     var checkbox: CGFloat = 2
     var container: CGFloat = 16
-    var indicator: CGFloat = MisticaRadiusConstants.roundedRadius
+    var indicator: CGFloat = 24
+    var chip: CGFloat = 24
+    var tag: CGFloat = 24
     var input: CGFloat = 12
     var legacyDisplay: CGFloat = 16
     var popup: CGFloat = 8

94f8fee70c169c431105052e5907fe0fda398be0
Alejandro Megías <Alejandro.Megias.Zapata@telefonica.com>
2025-09-30 12:34:41 +0200

    Import new brand

---
+++ Sources/MisticaCommon/Radius/Brands/MovistarNewCornerRadius.swift
@@ -0,0 +1,25 @@
+//
+//  MovistarNewCornerRadius.swift
+//
+//  Made with ❤️ by Novum
+//
+//  Copyright © Telefonica. All rights reserved.
+//
+
+import Foundation
+
+struct MovistarNewCornerRadius: MisticaCornerRadius {
+    var avatar: CGFloat = MisticaRadiusConstants.roundedRadius
+    var bar: CGFloat = MisticaRadiusConstants.roundedRadius
+    var button: CGFloat = 4
+    var checkbox: CGFloat = 4
+    var container: CGFloat = 16
+    var indicator: CGFloat = 24
+    var chip: CGFloat = 24
+    var tag: CGFloat = 0
+    var input: CGFloat = 12
+    var legacyDisplay: CGFloat = 16
+    var popup: CGFloat = 8
+    var sheet: CGFloat = 16
+    var mediaSmall: CGFloat = 8
+}

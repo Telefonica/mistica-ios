5cf6a29bbc93455953357e683192fb199c1a492e
Alejandro Megías <Alejandro.Megias.Zapata@telefonica.com>
2025-09-30 12:34:41 +0200

    Import new brand

---
+++ Sources/MisticaCommon/ThemeVariants/Brands/MovistarNewThemeVariants.swift
@@ -0,0 +1,14 @@
+//
+//  MovistarNewThemeVariants.swift
+//
+//  Made with ❤️ by Novum
+//
+//  Copyright © Telefonica. All rights reserved.
+//
+
+import Foundation
+
+struct MovistarNewThemeVariants: MisticaThemeVariants {
+    var successFeedback: MisticaThemeVariantType = .default
+    var brandLoadingScreen: MisticaThemeVariantType = .default
+}

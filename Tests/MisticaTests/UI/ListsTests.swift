56922b2d772de66e05073827dada8db347a79250
Daniel Illescas Romero <e.swiosdev3.cdo@telefonica.com>
2026-01-13 15:07:37 +0100

    test

--- Tests/MisticaTests/UI/ListsTests.swift
+++ Tests/MisticaTests/UI/ListsTests.swift
@@ -28,6 +28,24 @@ final class ListsTests: XCTestCase {
 
     // MARK: - Default config
 
+    func testCellWithTextAndImage() {
+        let listTestsViewController = makeMultiSectionListTestsViewController(numberOfRows: 2)
+
+        assertSnapshot(
+            of: listTestsViewController,
+            as: .image(on: .iPhoneSe),
+            named: "Before reloading"
+        )
+        
+        listTestsViewController.listView.reloadData()
+        
+        assertSnapshot(
+            of: listTestsViewController,
+            as: .image(on: .iPhoneSe),
+            named: "After reloading"
+        )
+    }
+    
     func testCellDefaultConfigAndImage() {
         let listTestsViewController = makeListTestsViewController(assetType: .custom(.image(AnyValues.image)))
 
@@ -921,6 +939,15 @@ extension ListsTests {
 
         return listTestsViewController
     }
+    
+    private func makeMultiSectionListTestsViewController(
+        listView: ListTableView = ListTableView(),
+        numberOfRows: Int = 2
+    ) -> MultiSectionListsTestsViewController {
+        let listTestsViewController = MultiSectionListsTestsViewController(listView: listView)
+        listTestsViewController.numberOfRows = numberOfRows
+        return listTestsViewController
+    }
 
     private func makeTestCell(
         title: String? = nil,
@@ -1079,3 +1106,57 @@ private class ListsTestsViewController: UIViewController, UITableViewDataSource 
         return cell
     }
 }
+
+private class MultiSectionListsTestsViewController: UIViewController, UITableViewDataSource {
+    let listView: ListTableView
+    var numberOfRows = 1
+
+    init(listView: ListTableView) {
+        self.listView = listView
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .background
+
+        view.addSubview(withDefaultConstraints: listView)
+        listView.dataSource = self
+        ListTableViewCell.register(on: listView)
+    }
+
+    func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
+        numberOfRows
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        2
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = ListTableViewCell.dequeueReusableCell(for: indexPath, from: tableView)
+
+        switch indexPath.section {
+        case 0:
+            cell.listCellContentView.title = "Aaa"
+            cell.listCellContentView.subtitle = nil
+            cell.listCellContentView.detailText = nil
+            cell.listCellContentView.assetType = .smallIcon(UIImage(color: .green))
+        case 1:
+            cell.listCellContentView.title = "Aaa"
+            cell.listCellContentView.subtitle = "Bbb"
+            cell.listCellContentView.detailText = "Ccc"
+            cell.listCellContentView.assetType = .smallIcon(UIImage(color: .red))
+        default:
+            fatalError()
+        }
+
+        return cell
+    }
+}
+
